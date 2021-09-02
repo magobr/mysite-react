32eba7d6e735e2074433e311c396991c53c942fa
Thiago Novaes <thiago.santos@estadao.com.br>
2021-09-01 23:23:39 -0300

    Initial Commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './button.css';
+
+class Button extends React.Component {
+
+    render(){
+        return(
+            <>
+                <button></button>
+            </>
+        )
+    }
+
+}
+
+export default Button;
