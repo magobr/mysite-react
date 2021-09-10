d79c0510acc05aa12376918f63a2ce902ff6a3bc
Thiago Novaes <thiago.santos@estadao.com.br>
2021-09-10 16:16:59 -0300

    Header Admin

--- src/components/headerAdmin/index.jsx
+++ src/components/headerAdmin/index.jsx
@@ -1,9 +1,29 @@
 import './style.css';
 
+import PerfilMenu from './menuDropDown';
+
 export default function Header() {
     return(
         <div className="header">
-            <h1>teste</h1>
+            <header>
+                <div class="top-container">
+                    <div class="img-perfil">
+                        <span class="first-letter">R</span>
+                    </div>
+                    <div class="name-perfil">
+                        <div class="userName">
+                            <p>Roberto Costa</p>
+                        </div>
+                    </div>
+                    < PerfilMenu />
+                    <div class="static-menu">
+                        <ul>
+                            <li><a href="/a">Criar Noticias</a></li>
+                            <li><a href="/a">Minhas Noticias</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
         </div>
     )
 }
